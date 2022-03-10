359eadc6e81d47a6095fd99217c8366e87db79d3
Maycon Braga <maycondbraga.s@gmail.com>
2022-03-09 23:36:01 -0300

    Add project files.

---
+++ src/Alura.LeilaoOnline.WebApp/Controllers/LeilaoController.cs
@@ -0,0 +1,138 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Alura.LeilaoOnline.WebApp.Dados;
+using Alura.LeilaoOnline.WebApp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Alura.LeilaoOnline.WebApp.Controllers
+{
+    public class LeilaoController : Controller
+    {
+
+        AppDbContext _context;
+
+        public LeilaoController()
+        {
+            _context = new AppDbContext();
+        }
+
+        #region Private Methods
+        private IEnumerable<Categoria> BuscarCategorias()
+        {
+            return _context.Categorias.ToList();
+        }
+
+        private Leilao BuscarLeilaoPorId(int id)
+        {
+            return _context.Leiloes.First(x => x.Id == id);
+        }
+        #endregion Private Methods
+
+
+        public IActionResult Index()
+        {
+            var leiloes = _context.Leiloes
+                .Include(l => l.Categoria);
+            return View(leiloes);
+        } 
+
+        [HttpGet]
+        public IActionResult Insert()
+        {
+            ViewData["Categorias"] = BuscarCategorias();
+            ViewData["Operacao"] = "Inclusão";
+            return View("Form");
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Leilao model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Leiloes.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewData["Categorias"] = BuscarCategorias();
+            ViewData["Operacao"] = "Inclusão";
+            return View("Form", model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewData["Categorias"] = BuscarCategorias();
+            ViewData["Operacao"] = "Edição";
+            var leilao = BuscarLeilaoPorId(id);
+            if (leilao == null) return NotFound();
+            return View("Form", leilao);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Leilao model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Leiloes.Update(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewData["Categorias"] = BuscarCategorias();
+            ViewData["Operacao"] = "Edição";
+            return View("Form", model);
+        }
+
+        [HttpPost]
+        public IActionResult Inicia(int id)
+        {
+            var leilao = BuscarLeilaoPorId(id);
+            if (leilao == null) return NotFound();
+            if (leilao.Situacao != SituacaoLeilao.Rascunho) return StatusCode(405);
+            leilao.Situacao = SituacaoLeilao.Pregao;
+            leilao.Inicio = DateTime.Now;
+            _context.Leiloes.Update(leilao);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Finaliza(int id)
+        {
+            var leilao = BuscarLeilaoPorId(id);
+            if (leilao == null) return NotFound();
+            if (leilao.Situacao != SituacaoLeilao.Pregao) return StatusCode(405);
+            leilao.Situacao = SituacaoLeilao.Finalizado;
+            leilao.Termino = DateTime.Now;
+            _context.Leiloes.Update(leilao);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Remove(int id)
+        {
+            var leilao = BuscarLeilaoPorId(id);
+            if (leilao == null) return NotFound();
+            if (leilao.Situacao == SituacaoLeilao.Pregao) return StatusCode(405);
+            _context.Leiloes.Remove(leilao);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpGet]
+        public IActionResult Pesquisa(string termo)
+        {
+            ViewData["termo"] = termo;
+            var leiloes = _context.Leiloes
+                .Include(l => l.Categoria)
+                .Where(l => string.IsNullOrWhiteSpace(termo) || 
+                    l.Titulo.ToUpper().Contains(termo.ToUpper()) || 
+                    l.Descricao.ToUpper().Contains(termo.ToUpper()) ||
+                    l.Categoria.Descricao.ToUpper().Contains(termo.ToUpper())
+                );
+            return View("Index", leiloes);
+        }
+    }
+}
